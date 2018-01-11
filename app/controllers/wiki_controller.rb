a5345a91428d283ebdc5d07ceb3040c08a9e144c
Mujadded <mujadded.alif@welltravel.com>
2018-01-11 13:21:11 +0600

    Stale page added in controller

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -306,6 +306,26 @@ class WikiController < ApplicationController
     @paginated = true
   end
 
+  def stale
+    @title = I18n.t('wiki_controller.wiki')
+
+    order_string = if params[:order] == 'alphabetic'
+                     'node_revisions.title ASC'
+                   else
+                     'node_revisions.timestamp ASC'
+                   end
+
+    @wikis = Node.includes(:revision)
+                 .references(:node_revisions)
+                 .group('node_revisions.nid')
+                 .order(order_string)
+                 .where("node_revisions.status = 1 AND node.status = 1 AND (type = 'page' OR type = 'tool' OR type = 'place')")
+                 .page(params[:page])
+                 
+    @paginated = true
+    render template: 'wiki/index'
+  end
+
   def popular
     @title = I18n.t('wiki_controller.popular_wiki_pages')
     @wikis = Node.limit(40)
